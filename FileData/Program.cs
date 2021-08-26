8eac06249b979419348f7fc409f4720a9d195158
Mahendra Upadhyay <mahendra.upadhyay@hp.com>
2021-08-26 16:51:18 +0530

    Add project files.

---
+++ FileData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileDetails fileDetails = new FileDetails();
+            if(args[0]=="-v")
+            {
+              Console.WriteLine(fileDetails.Version(args[1]));
+                Console.ReadKey();
+            }
+        }
+    }
+}
