7051ca288ef06e41ad85af13d6a9c5cf18d90a69
Mahendra Upadhyay <mahendra.upadhyay@hp.com>
2021-08-26 17:08:43 +0530

    Added Unit Test Project.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FileData;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Class1 class1 = new Class1();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] input = new string[] { "-v", "C:/test.txt" };
+            string result = class1.GetVersionNumber(input);
+            Assert.IsNotNull(result);
+        }
+    }
+}
